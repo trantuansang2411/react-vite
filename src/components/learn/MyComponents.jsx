96dedfbde58cda056d664d8ab627e5a5f4b320fe
trantuansang2411 <22693501.sang@student.iuh.edu.vn>
2026-01-12 23:41:50 +0700

    accompany css

--- src/components/learn/MyComponents.jsx
+++ src/components/learn/MyComponents.jsx
@@ -1,7 +1,14 @@
 // component = html + css + js
+// fragment
+import './style.css';
+
 const MyComponent = () => {
     return (
-        <div className='myComponent'>Sang Dep Trai</div> //tag
+        <>
+            <div className='myComponent'>Sang Dep Trai</div>
+            <div className="Child" style={{ fontFamily: "fantasy" }}>Con</div>
+        </>
+
     );
 }
 export default MyComponent;
