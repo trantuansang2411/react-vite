5d1ba3dd8a4689c66e04d89fa74d18e97747b615
trantuansang2411 <22693501.sang@student.iuh.edu.vn>
2026-01-12 23:23:45 +0700

    Components_v2

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-// component = html + css + js
-const MyComponent = () => {
-  return (
-    <div className='myComponent'>Sang Dep Trai</div> //tag
-  );
-}
-
+import MyComponent from './components/learn/MyComponents'
+import { SecondComponent, ThirdComponent } from './components/learn/SecondComponent'
 const App = () => {
   const [count, setCount] = useState(0)
 
@@ -25,6 +19,8 @@ const App = () => {
       </div>
       <h1>Hello World-TuanSang</h1>
       <MyComponent />
+      <SecondComponent />
+      <ThirdComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/learn/MyComponents.jsx
@@ -0,0 +1,7 @@
+// component = html + css + js
+const MyComponent = () => {
+    return (
+        <div className='myComponent'>Sang Dep Trai</div> //tag
+    );
+}
+export default MyComponent;
